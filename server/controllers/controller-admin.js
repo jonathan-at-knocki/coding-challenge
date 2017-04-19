59b49de0ddcac08a1c81bc0e2bb328cf8fdcc273
Jonathan Ganc <jonganc@gmail.com>
2017-04-19 19:34:18 -0400

    Debug mark ii

--- server/controllers/controller-admin.js
+++ server/controllers/controller-admin.js
@@ -7,44 +7,6 @@ const common = require('./_includes/controller-common');
 
 /*
 ///////////////////////////////////////////////////////////////////////////
-//   USER, QUIZ LOOKUP FUNCS
-*/
-
-// helper function that is passed to an express app callback and looks up
-// user from session variables, then either redirects to login or calls
-// callback with signature:
-//   callback(user, errArr)
-//
-// this should be the first function called by any routing path into the
-// admin console
-//
-// if no user, redirects to "/admin/login"
-// (thus, this cannot be used for /admin/login)
-function validateAndCall(req, res, callback) {
-  const errArr = [];
-  const redirect = () => {
-    req.session.userId = null;
-    res.redirect('/admin/login?redirect=' + encodeURI(req.url));
-  };
-  if (!req.session.userId) {
-    redirect();
-    return; // eslint-disable-line no-useless-return
-  } else { // eslint-disable-line no-else-return
-    userModel.findById(req.session.userId).exec((err, user) => {
-      common.addToErrArr(err, 'user', errArr);
-      if (err) {
-        redirect();
-        return; // eslint-disable-line no-useless-return
-      } else { // eslint-disable-line no-else-return
-        callback(user, errArr);
-        return; // eslint-disable-line no-useless-return
-      }
-    });
-  }
-}
-
-/*
-///////////////////////////////////////////////////////////////////////////
 //   LOGIN / LOGOUT
 */
 
@@ -91,7 +53,8 @@ exports.loginDo = function login(req, res) {
   redirectIfLoggedIn(req, res, () => {
     const email = req.body.email;
     const password = req.body.password;
-    const reshow = msg => exports.loginShow(req, res, null, email, [msg]);
+    const reshow
+          = msg => exports.loginShow(req, res, null, email, msg ? [msg] : []);
 
     if (!(email && password)) {
       reshow('Both email and password must be nonblank');
@@ -127,69 +90,65 @@ exports.logout = function logout(req, res, next) {
 
 /*
 ///////////////////////////////////////////////////////////////////////////
+//   USER, QUIZ LOOKUP FUNCS
+*/
+
+// helper function that looks up user from session variables, then either
+// redirects to login or calls callback with signature:
+//   callback(user, errArr)
+//
+// this should be the first function called by any routing path into the
+// admin console
+//
+// if no user, redirects to "/admin/login"
+// (thus, this cannot be used for /admin/login)
+function validateAndCall(req, res, callback) {
+  const errArr = [];
+  const redirect = () => {
+    req.session.userId = null;
+    res.redirect('/admin/login?redirect=' + encodeURI(req.url));
+  };
+  if (!req.session.userId) {
+    redirect();
+    return; // eslint-disable-line no-useless-return
+  } else { // eslint-disable-line no-else-return
+    userModel.findById(req.session.userId).exec((err, user) => {
+      common.addToErrArr(err, 'user', errArr);
+      if (err) {
+        redirect();
+        return; // eslint-disable-line no-useless-return
+      } else { // eslint-disable-line no-else-return
+        callback(user, errArr);
+        return; // eslint-disable-line no-useless-return
+      }
+    });
+  }
+}
+
+/*
+///////////////////////////////////////////////////////////////////////////
 //   THE ADMIN PAGES
 */
 
-// helper function which returns a function that can be passed to an
-//  express app callback and which checks credentials and then extracts
-//  quizzes, user from session variables before calling inner with
-//  signature:
-//     inner(req, res, user, quizzes, errArr)
-// where quizzes is an array of all quizzes
-
-function validateAndFindQuizzesAndCallInner(inner) {
-  return (req, res, next) => {
-    validateAndCall(req, res, (user, errArr) => {
-      quizModel.find().exec((err, quizzes) => {
-        common.addToErrArr(err, 'all quizzes', errArr);
-        inner(req, res, user, quizzes, errArr);
-      });
-    });
-  };
-}
-
-// like validateAndFindQuizzesAndCallInner but uses req.params.quizId to
-// find a single quiz instead of all quizzes
-function validateAndFindOneQuizAndCallInner(inner) {
-  return (req, res, next) => {
-    var errArr = [];
-    common.findByIdAndMore(
-      userModel, req.session.userId, 'user', errArr, (user) => {
-        quizModel.find().exec((err, quizzes) => {
-          common.addToErrArr(err, 'all quizzes', errArr);
-          inner(req, res, user, quizzes, errArr);
-        });
-      });
-  };
-}
-
-
 //  the main admin screen
-exports.main = function admin(req, res, next) {
-  quizModel.find().exec((err, quizzes) => {
-    res.render('view-quiz', {
-      quizzes,
-      errArr: err ? 'Could not find quiz: ' + err.toString() : ''
+exports.main = function main(req, res, next) {
+  validateAndCall(req, res, (user, errArr) => {
+    quizModel.find().exec((err, quizzes) => {
+      common.addToErrArr(err, 'all quizzes', errArr);
+      res.render('view-admin-main', { user, quizzes, errArr });
     });
   });
-  res.render('view-admin-main', { user: req.session.user });
 };
 
 //  the admin screen for one quiz
 exports.quiz = function quiz(req, res, next) {
-  if (!req.session.user) {
-    res.redirect('/admin/login');
-    return;
-  }
-
-  quizModel.findById(req.session.quizId).exec((err, quiz) => {
-    res.render('view-quiz', {
-      quiz,
-      errArr: err ? 'Could not find quiz: ' + err.toString() : ''
-    });
+  validateAndCall(req, res, (user, errArr) => {
+    common.findByIdAndMore(
+      quizModel, req.params.quizId, 'quiz', errArr, (quiz) => {
+        res.render('view-quiz', { user, quiz, errArr });
+      });
   });
 };
-
 
 /*
 ///////////////////////////////////////////////////////////////////////////
@@ -199,65 +158,55 @@ exports.quiz = function quiz(req, res, next) {
 //  registration screen
 //  email, errArr are for showing an error message and prepopulating the
 //  email box
-const registerShow = function registerShow(req, res, next, email, errArr) {
-  if (!req.session.user) {
-    res.redirect('/admin/login');
-    return;
-  }
-
-  if (req.session.user) {
+exports.registerShow = function registerShow(req, res, next, email, errArr) {
+  validateAndCall(req, res, (user, errArr) => {
     res.render('view-admin-register', {
-      title: 'Register new admin user',
+      user,
       email,
       errArr
     });
-  } else {
-    res.redirect('/admin/login');
-  }
+  });
 };
-exports.registerShow = registerShow;
+const registerShow = exports.registerShow;
 
 exports.registerAdd = function register(req, res) {
-  if (!req.session.user) {
-    res.redirect('/admin/login');
-    return;
-  }
+  validateAndCall(req, res, (user, errArr) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const reshow
+          = msg => registerShow(req, res, null, email, msg ? [msg] : []);
 
-  const email = req.body.email;
-  const password = req.body.password;
-  const reshow = msg => registerShow(req, res, null, email, msg);
+    // attempt to add user
+    if (!(email && password)) {
+      reshow('Both email and password must be non-blank');
+      return;
+    }
 
-  // attempt to add user
-
-  if (!(email && password)) {
-    reshow('Both email and password must be nonblank');
-    return;
-  }
-
-  // first check for existing user
-  userModel.findOne({ email }, (err, user) => {
-    if (err) {
-      reshow('Error searching for email: ' + err);
-    } else if (!user) {
-      // user not found. add him
-      userModel.create({ email }, (err, user) => {
-        if (err) {
-          reshow('Error adding user: ' + err);
-          return;
-        }
-        user.setPassword(password);
-        user.save((err) => {
+    // first check for existing user
+    userModel.findOne({ email }, (err, user) => {
+      if (err) {
+        reshow('Error searching for email: ' + err);
+      } else if (!user) {
+        // user not found. add him
+        userModel.create({ email }, (err, user) => {
           if (err) {
-            reshow('Error saving user password: ' + err);
+            reshow('Error adding user: ' + err);
             return;
           }
-          // all good. go to login
-          res.redirect('/admin/login');
+          user.setPassword(password);
+          user.save((err) => {
+            if (err) {
+              reshow('Error saving user password: ' + err);
+              return;
+            }
+            // all good
+            reshow();
+          });
         });
-      });
-    } else {
-      // email exists already
-      reshow('Email already exists in db');
-    }
+      } else {
+        // email exists already
+        reshow('Email already exists in db');
+      }
+    });
   });
 };
